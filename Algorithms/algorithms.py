c545568531d5244f8a204abb350bff1cf2ba8ce4
Rodney McGee <rodneymcgee73@gmail.com>
2023-03-20 02:44:07 -0500

    Add all_less_than_100 function for checking if all numbers in a list are less than 100. This commit adds the all_less_than_100 function which takes a list of numbers as a parameter and returns a boolean value indicationg whether all numbers are less than 100. The function has time complexity of O(n) because it iterates over each number in the input list once and returns False immediately if it finds a number greater than or equal to 100. Comment above function describes the time complexity.

--- Algorithms/algorithms.py
+++ Algorithms/algorithms.py
@@ -11,5 +11,15 @@ Leave a comment above the function stating the time complexity.
 def isEven(num):
     return num % 2 == 0
 
-print(isEven(4))
-print(isEven(7))
+print(isEven(4)) #should print True
+print(isEven(7)) #should print False
+
+# Time complexity: O(1)
+def all_less_than_100(numbers):
+    for num in numbers:
+        if num >= 100:
+            return False
+    return True
+
+print(all_less_than_100([1, 2, 3, 99])) #should print True
+print(all_less_than_100([1, 2, 3, 100])) #should print False

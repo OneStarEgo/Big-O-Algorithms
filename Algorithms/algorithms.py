e5e70680edfdeca6ce912468711515d09ba39e38
Rodney McGee <rodneymcgee73@gmail.com>
2023-03-20 09:59:00 -0500

    Add sort_list function for sorting list without built-in method. The sort_list function takes in a list of numbers and manually sorts them using a nested loop to compare each pair of adjacent numbers and swap them if they are not in order. This process is repeated until the entire list is sorted. Included a commit above the function stating the time complexity (O(n^2)).

--- Algorithms/algorithms.py
+++ Algorithms/algorithms.py
@@ -1,20 +1,12 @@
-"""
-Even or Odd
-Given a numeric value, determine if it is even or odd.
 
-The function should take the value in as a parameter and return a boolean value (True if even, False if odd).
-
-Leave a comment above the function stating the time complexity.
-"""
-
-# Time complexity: O(1)
+# Time complexity: O(1) - Linear
 def isEven(num):
     return num % 2 == 0
 
 print(isEven(4)) #should print True
 print(isEven(7)) #should print False
 
-# Time complexity: O(1)
+# Time complexity: O(1) - Linear
 def all_less_than_100(numbers):
     for num in numbers:
         if num >= 100:
@@ -24,7 +16,7 @@ def all_less_than_100(numbers):
 print(all_less_than_100([1, 2, 3, 99])) #should print True
 print(all_less_than_100([1, 2, 3, 100])) #should print False
 
-# Time complexity: O(1)
+# Time complexity: O(1) - Linear
 def has_repeated_names(names):
     name_counts = {}
     for name in names:
@@ -35,3 +27,16 @@ def has_repeated_names(names):
 
 print(has_repeated_names(["Joe", "Paul", "Gregor", "Ronald"])) #should print False
 print(has_repeated_names(["Joe", "Ronald", "Gregor", "Joe"])) #should print True
+
+# Time complexity: O(n^2) - Quadratic
+def sort_list(arr):
+    for i in range(len(arr)):
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[i]:
+                arr[i], arr[j] = arr[j], arr[i]
+    return arr
+
+unsorted = [6, 8, 3, 4, 7, 2]
+sorted = sort_list(unsorted)
+
+print(sorted) #should print [2, 3, 4, 6, 7, 8]
